8a978dabf47002849b29472fdfb8665ccd486f7d
Danilo Diez <daniloo10.dd@gmail.com>
2023-05-09 20:22:47 -0300

    add the error response

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -25,9 +25,9 @@ export default async function handler(
 ) {
   const prompt = req.body;
   try {
-    // const img = await fetchImages(prompt.prompt);
-    return res.status(200).json({ img: `![Image](https://oaidalleapiprodscus.blob.core.windows.net/private/org-Na0TdtbgraQ9gI0cVWL8rnIb/user-te9nrtxST05bPNI4H82QFZal/img-ePZCmRv9QEXjM1fqJe1I3Qed.png?st=2023-05-09T21%3A58%3A24Z&se=2023-05-09T23%3A58%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-09T20%3A17%3A35Z&ske=2023-05-10T20%3A17%3A35Z&sks=b&skv=2021-08-06&sig=5WbC%2BzV7fHUfaNuVsk93W0JORMC47FYuAdov03C2AYY%3D)` });
+    const img = await fetchImages(prompt.prompt);
+    return res.status(200).json({ img: `![Image](${img})` });
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(400).json({error});
   }
 }
